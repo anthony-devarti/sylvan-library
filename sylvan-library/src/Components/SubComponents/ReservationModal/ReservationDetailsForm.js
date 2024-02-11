e265798a1c3a570ac95af0ddab1ce7bb932cfaf4
anthony-devarti <anthony.devarti@gmail.com>
2024-02-10 20:42:32 -0500

    res submission handles form

--- sylvan-library/src/Components/SubComponents/ReservationModal/ReservationDetailsForm.js
+++ sylvan-library/src/Components/SubComponents/ReservationModal/ReservationDetailsForm.js
@@ -1,82 +1,135 @@
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import { Col, Row, Container, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { getCurrentDateTime } from '../../../utilities/dateTimeFormatter';
 
-export default function ReservationDetailsForm({ onSubmit }) {
-    const formik = useFormik({
-        initialValues: {
-            note: '',
-            pickupMethod: 'from-lenders-home',
-            returnDate: '',
-        },
-        validationSchema: Yup.object({
-            note: Yup.string().required('Note is required'),
-            pickupMethod: Yup.string().required('Pickup Method is required'),
-            returnDate: Yup.string().required('Return Date is required'),
-        }),
-        onSubmit: (values) => {
-            // You can handle the form submission here
-            console.log('Form Data:', values);
-            // Perform additional actions, e.g., API calls, dispatching actions, etc.
-        },
-    });
-
+/**
+ * ReservationDetailsForm Component
+ * @param {Object} props - React component props
+ * @param {Object} props.formik - Formik instance for form state management
+ */
+const ReservationDetailsForm = ({ formik }) => {
     return (
         <form onSubmit={formik.handleSubmit} className="reservation-form">
-            <div className='left-form'>
-                <div className="form-group">
-                    <label htmlFor="pickupMethod">Pickup Method:</label>
-                    <select
-                        id="pickupMethod"
-                        name="pickupMethod"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.pickupMethod}
-                    >
-                        <option value="from-lenders-home">From Lender's Home</option>
-                        <option value="at-specific-event">At Specific Event</option>
-                        <option value="other">Other</option>
-                    </select>
-                    {formik.touched.pickupMethod && formik.errors.pickupMethod ? (
-                        <div className="error-message">{formik.errors.pickupMethod}</div>
-                    ) : null}
-                </div>
+            <Container>
+                <Row>
+                    {/* Left column */}
+                    <Col xs={6}>
+                        {/* Pickup Method dropdown */}
+                        <OverlayTrigger
+                            placement="top"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={
+                                <Tooltip id="button-tooltip" >
+                                    Choose how you are going to collect your reservation.  If you choose other, all necessary information must be included in the Note section.
+                                </Tooltip>
+                            }
+                        >
+                            <div className="form-group">
+                                <label htmlFor="pickupMethod">Pickup Method:</label>
+                                <select
+                                    id="pickupMethod"
+                                    name="pickupMethod"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.pickupMethod}
+                                >
+                                    <option value="from-lenders-home">From Lender's Home</option>
+                                    <option value="at-specific-event">At Specific Event</option>
+                                    <option value="other">Other</option>
+                                </select>
+                                {formik.touched.pickupMethod && formik.errors.pickupMethod ? (
+                                    <div className="error-message">{formik.errors.pickupMethod}</div>
+                                ) : null}
+                            </div>
+                        </OverlayTrigger>
 
-                <div className="form-group">
-                    <label htmlFor="returnDate">Return Date:</label>
-                    <input
-                        type="date"
-                        id="returnDate"
-                        name="returnDate"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.returnDate}
-                    />
-                    {formik.touched.returnDate && formik.errors.returnDate ? (
-                        <div className="error-message">{formik.errors.returnDate}</div>
-                    ) : null}
-                </div>
-            </div>
-            <div className='right-form'>
-                <div className="form-group">
-                    <label htmlFor="note">Note:</label>
-                    <textarea
-                        id="note"
-                        name="note"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.note}
-                    />
-                    {formik.touched.note && formik.errors.note ? (
-                        <div className="error-message">{formik.errors.note}</div>
-                    ) : null}
-                </div>
+                        {/* Note textarea */}
+                        <OverlayTrigger
+                            placement="bottom"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={
+                                <Tooltip id="button-tooltip" >
+                                    This is your only chance to add notes to this reservation.  Be sure to include everything you need to in order to be able to collect and return your reservation.  If the lender is meeting you somewhere, it should be noted in here.
+                                </Tooltip>
+                            }
+                        >
+                            <div className="form-group">
+                                <label htmlFor="note">Note:</label>
+                                <textarea
+                                    id="note"
+                                    name="note"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.note}
+                                    placeholder='Include information about the pickup. If you are picking up at an event, mention the event.'
+                                />
+                                {formik.touched.note && formik.errors.note ? (
+                                    <div className="error-message">{formik.errors.note}</div>
+                                ) : null}
+                            </div>
+                        </OverlayTrigger>
+                    </Col>
 
-                <div className="form-group">
-                    <button type="submit" className="submit-button">
-                        Save
-                    </button>
-                </div>
-            </div >
+                    {/* Right column */}
+                    <Col xs={6}>
+                        {/* Pickup Date and Time input - Enabled only for "From Lender's Home" */}
+                        <OverlayTrigger
+                            placement="top"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={
+                                <Tooltip id="button-tooltip" >
+                                    If you are not picking up at an event, you should select a pickup date and time.  Failure to meet this time may result in your reservation being cancelled.
+                                </Tooltip>
+                            }
+                        >
+                            <div className="form-group">
+                                <label htmlFor="pickupDateTime">Pickup Date and Time:</label>
+                                <input
+                                    type="datetime-local"
+                                    id="pickupDateTime"
+                                    name="pickupDateTime"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.pickupDateTime}
+                                    disabled={formik.values.pickupMethod !== 'from-lenders-home'}
+                                    min={getCurrentDateTime()}
+                                />
+                                {formik.touched.pickupDateTime && formik.errors.pickupDateTime ? (
+                                    <div className="error-message">{formik.errors.pickupDateTime}</div>
+                                ) : null}
+                            </div>
+                        </OverlayTrigger>
+
+                        {/* Return Date input */}
+                        <OverlayTrigger
+                            placement="bottom"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={
+                                <Tooltip id="button-tooltip" >
+                                    Pick the return time.  Keep in mind that reservations that are returned more than a week after pickup are more likely to be declined, especially for new borrowers.  This time is binding, so please pick a date and time carefully.  It is your responsibility to return your reservation by this time.  Failure to do so may result in your suspension from the program.
+                                </Tooltip>
+                            }
+                        >
+                            <div className="form-group">
+                                <label htmlFor="returnDate">Return Date:</label>
+                                <input
+                                    type="datetime-local"
+                                    id="returnDate"
+                                    name="returnDate"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.returnDate}
+                                    min={getCurrentDateTime()}
+                                />
+                                {formik.touched.returnDate && formik.errors.returnDate ? (
+                                    <div className="error-message">{formik.errors.returnDate}</div>
+                                ) : null}
+                            </div>
+                        </OverlayTrigger>
+                    </Col>
+                </Row>
+            </Container>
         </form>
     );
 }
+
+export default ReservationDetailsForm;
--- sylvan-library/src/apiActions/createReservation.js
+++ sylvan-library/src/apiActions/createReservation.js
@@ -4,32 +4,53 @@ import { errorToast, successToast } from '../Components/SubComponents/Toastify';
 import { setCurrentReservation, setReservations } from '../features/basket/basketSlice';
 
 const createReservation = (userID) => async (dispatch) => {
-    try {
-      const response = await axios.post(`${baseURL}reservation/`, {
-        "id_user": userID,
-        "date_created": "2024-01-21T12:34:56",
-        "return_date": "2025-01-21T12:30:45.123456",
-        "action_required": 1,
-        "stage" : reservationStage.unsubmitted
-      });
-  
-      // Assuming the response.data has the necessary reservation information
-      const newReservation = response.data;
-  
-      successToast('Reservation is ready');
-      
-      // Dispatch the new reservation data to the Redux store
-      dispatch(setCurrentReservation([newReservation]));
-  
-      // Return the newReservation for potential further use
-      return newReservation;
-    } catch (error) {
-      console.error('Error:', error);
-      errorToast('An error occurred trying to create a new reservation.');
-      throw error; // Re-throw the error so that the calling code can handle it if needed
+  try {
+    // Check for existing reservations matching the criteria
+    const response = await axios.get(`${baseURL}reservation/`, {
+      params: {
+        id_user: userID,
+        stage: reservationStage.unsubmitted,
+      },
+    });
+
+    const existingReservations = response.data.results;
+
+    // Log the response data and its type for debugging
+    console.log('Response Data:', existingReservations, 'Type:', typeof existingReservations);
+
+    if (existingReservations.length > 0) {
+      // If there's an existing reservation, set it as the current reservation
+      const existingReservation = existingReservations[0];
+      dispatch(setCurrentReservation([existingReservation]));
+
+      successToast('Existing reservation found.');
+      return existingReservation;
     }
-  };
 
+    // No existing reservation found, proceed to create a new one
+    const newReservationResponse = await axios.post(`${baseURL}reservation/`, {
+      id_user: userID,
+      date_created: "2024-01-21T12:34:56",
+      return_date: "2025-01-21T12:30:45.123456",
+      action_required: 1,
+      stage: reservationStage.unsubmitted,
+    });
 
+    // Assuming the newReservationResponse.data has the necessary reservation information
+    const newReservation = newReservationResponse.data;
 
-export default createReservation
+    successToast('New reservation created.');
+
+    // Dispatch the new reservation data to the Redux store
+    dispatch(setCurrentReservation([newReservation]));
+
+    // Return the newReservation for potential further use
+    return newReservation;
+  } catch (error) {
+    console.error('Error:', error);
+    errorToast('An error occurred trying to create or retrieve a reservation.');
+    throw error; // Re-throw the error so that the calling code can handle it if needed
+  }
+};
+
+export default createReservation;
