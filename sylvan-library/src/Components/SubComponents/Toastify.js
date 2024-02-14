72d2d46c2f9720bc67722847e0c69e6f7f712926
anthony-devarti <anthony.devarti@gmail.com>
2024-02-13 23:02:22 -0500

    table flip in toast error handling

--- sylvan-library/src/Components/SubComponents/Toastify.js
+++ sylvan-library/src/Components/SubComponents/Toastify.js
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 function stringCheck(text){
     if (typeof text != 'string'){
-        return 'Someone tried to put something besides a string in here, probably Anthony'
+        return 'Someone tried to put something besides a string in here, probably Anthony ʕノ•ᴥ•ʔノ ︵ ┻━┻'
     }
     //maybe something that checks for string length here?
 
